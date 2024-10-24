9bd82c3c96ffd521bdca52fcc4b43367ffd96662
SrinjanaNag12 <srinjananag68@gmail.com>
2024-10-23 21:42:56 -0400

    commit1

--- aws-packer/install.sh
+++ aws-packer/install.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
-# Update the instance and install necessary packages
+# Update the instance and install dependencies
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
-# Install Python and pip
-sudo apt-get install -y python3-pip python3-venv
+# Install Python and venv
+sudo apt-get install -y python3 python3-venv
 
-# Create a directory for the Flask app and move files
+# Create a directory for the Flask app
 sudo mkdir -p /var/www/html/api
 
-# Move the Flask app files from /tmp to the target directory
+# Move the application files to the directory
 sudo mv /tmp/*.py /var/www/html/api/
 sudo mv /tmp/requirements.txt /var/www/html/api/
-sudo mv /home/ubuntu/.env /var/www/html/api/
 
-# Create a Python virtual environment in the target directory
+# Create a virtual environment
 python3 -m venv /var/www/html/api/venv
 
-# Activate the virtual environment and install the required packages
+# Activate the virtual environment
 source /var/www/html/api/venv/bin/activate
+
+# Install dependencies within the virtual environment
 pip install --no-cache-dir -r /var/www/html/api/requirements.txt
 
-
-# sudo systemctl enable mysql
+# Deactivate the virtual environment (optional, but good practice)
+deactivate

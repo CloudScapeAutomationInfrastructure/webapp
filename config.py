bd9b3e867bfea45fbdf827db1ed229693323911b
SrinjanaNag12 <srinjananag68@gmail.com>
2024-10-29 23:11:19 -0400

    update again

--- config.py
+++ config.py
@@ -7,3 +7,4 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
+    S3_BUCKET_NAME = os.getenv('S3_BUCKET_NAME')

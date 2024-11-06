e32f6749092f8b32b6b87374c9ace84b4fb001ce
SrinjanaNag12 <srinjananag68@gmail.com>
2024-11-06 03:01:44 -0500

    check the updated changes

--- routes.py
+++ routes.py
@@ -54,7 +54,7 @@ def send_email(subject, content, to_email):
     except Exception as e:
         logger.error(f"Failed to send email: {str(e)}")
 
-@user_routes.route('/user', methods=['POST'])
+@user_routes.route('/v1/user', methods=['POST'])
 def create_user():
     try:
         data = request.form
@@ -99,7 +99,7 @@ def create_user():
         logger.error(f"Error: {str(e)}")
         return jsonify({"error": "An internal server error occurred"}), 500
 
-@user_routes.route('/user', methods=['GET'])
+@user_routes.route('/v1/user', methods=['GET'])
 @auth.login_required
 def get_user():
     try:
@@ -115,7 +115,7 @@ def get_user():
         logger.error(f"Failed to retrieve user: {str(e)}")
         return jsonify({"error": "Failed to retrieve user"}), 500
 
-@user_routes.route('/user/self/pic', methods=['DELETE'])
+@user_routes.route('/v1/user/self/pic', methods=['DELETE'])
 @auth.login_required
 def delete_image():
     try:
@@ -138,7 +138,7 @@ def delete_image():
         send_email("Image Deletion Failed", f"Your image deletion failed due to an error: {str(e)}", user.email)
         return jsonify({"error": "Failed to delete image"}), 500
 
-@user_routes.route('/healthz', methods=['GET'])
+@user_routes.route('/v1/healthz', methods=['GET'])
 def health_check():
     try:
         put_custom_metric('HealthCheck', 1)
